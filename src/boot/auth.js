912d9321e5b28271ee63d07c555c1205f2f1b2ae
Marcel <masv122@gmail.com>
2020-08-19 11:14:32 -0400

    fix(deploy) arreglado bug en el auth

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -2,8 +2,9 @@ import { LocalStorage } from "quasar";
 
 export default ({ store }) => {
 	let perfilLocal = LocalStorage.getItem("perfil");
-	if (!!perfil) {
-		store.commit("perfiles/updatePerfil", perfil);
+	if (!!perfilLocal) {
 		let perfil = store.getters["perfiles/buscarPerfil"](perfilLocal.id);
+		if (!!perfil)
+			store.commit("perfiles/updatePerfil", perfil);
 	}
 };
