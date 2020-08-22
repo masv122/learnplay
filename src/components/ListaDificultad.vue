cf8c92e3afaf477316705b199c4ffd7eb0fb9c89
Marcel <masv122@gmail.com>
2020-08-21 22:45:21 -0400

    fix(estadisticas): estadisticas agregadas

--- src/components/ListaDificultad.vue
+++ src/components/ListaDificultad.vue
@@ -5,7 +5,7 @@
 				<q-icon name="history" />
 			</q-item-section>
 			<q-item-section>Tiempo jugado</q-item-section>
-			<q-item-section side>00:00</q-item-section>
+			<q-item-section side>{{ tiempo }}</q-item-section>
 		</q-item>
 
 		<q-item clickable v-ripple active-class="text-orange">
@@ -13,7 +13,7 @@
 				<q-icon name="check" />
 			</q-item-section>
 			<q-item-section>Retos acertados</q-item-section>
-			<q-item-section side>0</q-item-section>
+			<q-item-section side>{{ correctas }}</q-item-section>
 		</q-item>
 
 		<q-item clickable v-ripple active-class="bg-teal-1 text-grey-8">
@@ -21,7 +21,7 @@
 				<q-icon name="clear" />
 			</q-item-section>
 			<q-item-section>Retos fallados</q-item-section>
-			<q-item-section side>0</q-item-section>
+			<q-item-section side>{{ incorrectas }}</q-item-section>
 		</q-item>
 
 		<q-item clickable v-ripple active-class="bg-teal-1 text-grey-8">
@@ -29,7 +29,7 @@
 				<q-icon name="block" />
 			</q-item-section>
 			<q-item-section>Retos pasados</q-item-section>
-			<q-item-section side>0</q-item-section>
+			<q-item-section side>{{ pasadas }}</q-item-section>
 		</q-item>
 
 		<q-item clickable v-ripple active-class="bg-teal-1 text-grey-8">
@@ -37,7 +37,7 @@
 				<q-icon name="offline_bolt" />
 			</q-item-section>
 			<q-item-section>Veces usado poderes</q-item-section>
-			<q-item-section side>0</q-item-section>
+			<q-item-section side>{{ poderes }}</q-item-section>
 		</q-item>
 
 		<q-item clickable v-ripple active-class="bg-teal-1 text-grey-8">
@@ -47,9 +47,8 @@
 			<q-item-section>Logros conseguidos</q-item-section>
 			<q-item-section side>
 				<q-knob
-					:min="5"
 					:max="10"
-					v-model="value1"
+					v-model="logros.length"
 					show-value
 					size="50px"
 					:thickness="0.22"
@@ -65,6 +64,38 @@
 <script>
 	export default {
 		name: "ListaDificultad",
+		props: {
+			tiempo: {
+				type: Number,
+				required: true,
+				default: 0,
+			},
+			correctas: {
+				type: Number,
+				required: true,
+				default: 0,
+			},
+			incorrectas: {
+				type: Number,
+				required: true,
+				default: 0,
+			},
+			pasadas: {
+				type: Number,
+				required: true,
+				default: 0,
+			},
+			poderes: {
+				type: Number,
+				required: true,
+				default: 0,
+			},
+			logros: {
+				type: Array,
+				required: true,
+				default: () => [],
+			},
+		},
 		data() {
 			return {
 				value1: 6,
