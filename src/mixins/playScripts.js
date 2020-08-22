e1ad8b0a02330747ed8c40672ea879a3ea51e1a6
Marcel <masv122@gmail.com>
2020-08-21 22:50:24 -0400

    engine(play) mejorado el sistema de prubas

--- src/mixins/playScripts.js
+++ src/mixins/playScripts.js
@@ -1,7 +1,15 @@
 import { mapState, mapGetters, mapMutations } from "vuex";
 export default {
+  watch: {
+    segundos(val) {
+      if (val === 60) {
+        this.updateSegundos(0);
+        this.increaseMinutos();
+      }
+    },
+  },
   computed: {
-    ...mapState("global", [
+    ...mapState("play", [
       "banderas",
       "opcionMultiple",
       "banderasGeneradas",
@@ -10,10 +18,13 @@ export default {
       "pruebaGenerada",
       "opciones",
       "correctas",
-      "falsas",
+      "incorrectas",
       "pasadas",
+      "resumenFinal",
     ]),
-    ...mapGetters("global", [
+    ...mapState("puntaje", ["correctas", "falsas", "pasadas"]),
+    ...mapState("cronometro", ["segundos", "minutos", "timer"]),
+    ...mapGetters("play", [
       "buscarPruebaBandera",
       "buscarPruebaOpcionMultiple",
     ]),
@@ -35,7 +46,7 @@ export default {
     },
   },
   methods: {
-    ...mapMutations("global", [
+    ...mapMutations("play", [
       "updateBanderasGeneradas",
       "updateMultiplesGeneradas",
       "updateTipoPrueba",
@@ -44,11 +55,34 @@ export default {
       "insertarBanderasGeneradas",
       "insertarMultiplesGeneradas",
       "updateCorrectas",
-      "updateFalsas",
+      "updateIncorrectas",
       "updatePasadas",
       "increaseCorrectas",
       "increaseIncorrectas",
       "increasePasadas",
+      "updateResumenFinal",
+    ]),
+    ...mapMutations("cronometro", [
+      "increaseSegundos",
+      "increaseMinutos",
+      "updateSegundos",
+      "updateMinutos",
+      "updateTimer",
+      "clearTimer",
+    ]),
+    ...mapMutations("puntaje", [
+      "updateCorrectas",
+      "updateIncorrectas",
+      "updatePasadas",
+      "increaseCorrectas",
+      "increaseIncorrectas",
+      "increasePasadas",
+    ]),
+    ...mapMutations("perfiles", [
+      "increaseExp",
+      "increaseCorrectasPerfil",
+      "increaseIncorrectasPerfil",
+      "increasePasadasPerfil",
     ]),
     ramdon(min, max) {
       return Math.floor(Math.random() * (max - min) + min);
@@ -116,14 +150,26 @@ export default {
       let resultado =
         this.mayusculas(respuestaCorrecta) === this.mayusculas(respuesta);
       this.notificacion(resultado);
-      if (resultado) this.increaseCorrectas();
-      else this.increaseIncorrectas();
-
+      if (resultado) this.correcta();
+      else this.incorrecta();
       this.generarPrueba();
+    },
+    correcta() {
+      this.increaseCorrectas();
+      this.increaseCorrectasPerfil(0);
+      this.increaseExp(50);
+    },
+    incorrecta() {
+      this.increaseIncorrectas();
+      this.increaseIncorrectasPerfil(0);
     },
     pasar() {
       this.increasePasadas();
+      this.increasePasadasPerfil(0);
       this.generarPrueba();
+    },
+    upExp() {
+      this.increaseExp(10);
     },
     reset() {
       this.updateBanderasGeneradas([]);
@@ -132,8 +178,23 @@ export default {
       this.updatePruebaGenerada(null);
       this.updateOpciones(null);
       this.updateCorrectas(0);
-      this.updateFalsas(0);
+      this.updateIncorrectas(0);
       this.updatePasadas(0);
+      this.clearTimer();
+      this.updateResumenFinal();
+    },
+    iniciarCronometro() {
+      if (this.segundos === 0)
+        this.updateTimer(
+          setInterval(() => {
+            this.increaseSegundos();
+          }, 1000)
+        );
+    },
+    reiniciarCronometro() {
+      clearInterval(this.timer);
+      this.updateSegundos(0);
+      this.updateMinutos(0);
     },
   },
 };
