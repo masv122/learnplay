a5ff7c938fa5845ea782b85594df9283d2e6accb
Marcel <masv122@gmail.com>
2020-08-21 22:48:31 -0400

    add(perfil) agregado componente perfil

--- src/components/PlayHeader.vue
+++ src/components/PlayHeader.vue
@@ -1,36 +1,14 @@
 <template>
 	<q-header elevated>
 		<div class="row no-wrap hide">
-			<q-toolbar class="col-9">
-				<q-knob
-					show-value
-					class="text-white q-ma-md"
-					v-model="value"
-					size="60px"
-					:thickness="0.2"
-					color="teal"
-					readonly
-					track-color="transparent"
-				>
-					<q-avatar
-						size="xl"
-						color="white"
-						text-color="indigo-9"
-						icon="perm_identity"
-					>
-						<q-badge floating color="teal">1</q-badge>
-					</q-avatar>
-				</q-knob>
+			<q-toolbar class="col-4">
+				<perfil-miniatura />
+			</q-toolbar>
+			<q-toolbar class="col-6">
 				<q-toolbar-title>Basico</q-toolbar-title>
 			</q-toolbar>
-			<q-toolbar class="col-3">
-				<q-btn
-					:to="{ name: 'Inicio' }"
-					color="negative"
-					dense
-					@click="reset()"
-					label="terminar"
-				/>
+			<q-toolbar class="col-2">
+				<q-btn @click="reset()" icon="close" flat round dense />
 			</q-toolbar>
 		</div>
 	</q-header>
@@ -38,10 +16,12 @@
 
 <script>
 	import playScripts from "src/mixins/playScripts";
+	import perfilMiniatura from "components/perfilMiniatura";
+
 	export default {
 		name: "SupHeader",
 		mixins: [playScripts],
-		components: {},
+		components: { perfilMiniatura },
 		data() {
 			return {
 				value: 81,
