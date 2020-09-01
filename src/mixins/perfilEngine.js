de4953b2c50c404fcbf0abdca4302ee487f3d7c3
Marcel <masv122@gmail.com>
2020-08-31 20:24:00 -0400

    refactor(login): ahora solo sigue los cambios en las estadisticas provisionales

--- src/mixins/perfilEngine.js
+++ src/mixins/perfilEngine.js
@@ -1,49 +1,19 @@
 import { mapState, mapGetters, mapMutations } from "vuex";
 export default {
   watch: {
-    perfil: {
-      deep: true,
-      immediate: true,
-      async handler(val) {
-        if (val.exp >= this.calculoExpMax) {
-          this.updateExp(this.perfil.exp - this.calculoExpMax);
-          this.updateNivel();
-          try {
-            await this.$db.local.rel.save("perfil", val);
-            this.$q.localStorage.set("perfil", val);
-          } catch (e) {
-            console.log("no se pudo guardar el perfil: " + e);
-          }
-        }
-      },
-    },
-    "perfil.exp": {
-      async handler() {
-        try {
-          await this.$db.local.rel.save("perfil", this.perfil);
-          this.$q.localStorage.set("perfil", this.perfil);
-        } catch (e) {
-          console.log("no se pudo guardar el perfil: " + e);
-        }
-      },
-    },
-    "perfil.estadisticas": {
-      deep: true,
-      async handler() {
-        try {
-          await this.$db.local.rel.save("perfil", this.perfil);
-          this.$q.localStorage.set("perfil", this.perfil);
-        } catch (e) {
-          console.log("no se pudo guardar el perfil: " + e);
-        }
-      },
+    exp(val) {
+      if (!!val && val >= this.expMax) {
+        console.log(this.exp - this.expMax);
+        this.updateExp(this.exp - this.expMax);
+        this.increaseNivel();
+      }
     },
   },
   computed: {
-    ...mapState("perfiles", ["perfil"]),
-    ...mapGetters("perfiles", ["calculoExpMax"]),
+    ...mapState("puntaje", ["exp"]),
+    ...mapGetters("puntaje", ["expMax"]),
   },
   methods: {
-    ...mapMutations("perfiles", ["updateNivel", "updateExp"]),
+    ...mapMutations("puntaje", ["increaseNivel", "updateExp"]),
   },
 };
