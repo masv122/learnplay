fbc4945fe7872ac94c42351844263432d06b0a65
Marcel <masv122@gmail.com>
2020-08-31 20:22:14 -0400

    refactor(estilo, components): estilo cambiado y componentes asincronos

--- src/components/SupHeader.vue
+++ src/components/SupHeader.vue
@@ -1,35 +1,47 @@
 <template>
-	<q-header class="bg-indigo-9">
-		<div class="row no-wrap hide">
-			<q-toolbar class="col-4">
-				<perfil-miniatura />
-			</q-toolbar>
-			<q-toolbar class="col-6">
-				<q-toolbar-title>Learn N' Play</q-toolbar-title>
-			</q-toolbar>
-			<q-toolbar class="col-2">
-				<q-btn flat round dense icon="more_vert">
-					<header-menu />
-				</q-btn>
-			</q-toolbar>
-		</div>
+	<q-header elevated>
+		<q-toolbar>
+			<perfil-miniatura />
+			<q-toolbar-title>Learn N' Play</q-toolbar-title>
+			<q-btn
+				flat
+				round
+				dense
+				color="white"
+				@click="$q.fullscreen.toggle()"
+				:icon="
+					$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'
+				"
+			/>
+			<q-btn flat round dense icon="more_vert">
+				<header-menu />
+			</q-btn>
+		</q-toolbar>
 		<q-tabs v-model="_tab">
 			<q-tab name="basico" label="Basico" />
 			<q-tab name="intermedio" label="Intermedio" />
 			<q-tab name="avanzado" label="Avanzado" />
 		</q-tabs>
 	</q-header>
+	<!-- 	<q-header class="bg-indigo-9">
+		<div class="row no-wrap hide">
+			<q-toolbar dense class="col-4">
+
+			</q-toolbar>
+			<q-toolbar class="col-2">
+			</q-toolbar>
+		</div>
+
+	</q-header> -->
 </template>
 
 <script>
 	import { mapState, mapMutations } from "vuex";
-	import perfilMiniatura from "components/perfilMiniatura";
-	import HeaderMenu from "components/HeaderMenu.vue";
 	export default {
 		name: "SupHeader",
 		components: {
-			HeaderMenu,
-			perfilMiniatura,
+			HeaderMenu: () => import("components/HeaderMenu.vue"),
+			perfilMiniatura: () => import("components/perfilMiniatura.vue"),
 		},
 		data() {
 			return {
