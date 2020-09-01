59a10c7fa3b4d13de75d18eafe297376cb258120
Marcel <masv122@gmail.com>
2020-08-31 20:16:06 -0400

    fix(visual): cambios menores

--- src/components/NuevoPerfil.vue
+++ src/components/NuevoPerfil.vue
@@ -62,7 +62,11 @@
 		<q-step :name="4" title="Confirmacion" icon="verified">
 			<confirmacion />
 			<q-stepper-navigation>
-				<q-btn color="primary" @click="guardarPerfil()" label="Listo" />
+				<q-btn
+					color="primary"
+					@click="crearNuevoPerfil"
+					label="Listo"
+				/>
 				<q-btn
 					flat
 					@click="updateStep(3)"
@@ -76,18 +80,17 @@
 </template>
 
 <script>
-	import { mapMutations, mapState } from "vuex";
-	import Genero from "components/nuevoPerfil/Genero.vue";
-	import InformacionPersonal from "components/nuevoPerfil/InformacionPersonal.vue";
-	import Foto from "components/nuevoPerfil/Foto.vue";
-	import Confirmacion from "components/nuevoPerfil/Confirmacion.vue";
+	import { mapMutations, mapState, mapActions, mapGetters } from "vuex";
+	import Perfil from "src/class/perfil";
 	export default {
 		name: "NuevoPerfil",
 		components: {
-			Genero,
-			InformacionPersonal,
-			Foto,
-			Confirmacion,
+			Genero: () => import("components/nuevoPerfil/Genero.vue"),
+			InformacionPersonal: () =>
+				import("components/nuevoPerfil/InformacionPersonal.vue"),
+			Foto: () => import("components/nuevoPerfil/Foto.vue"),
+			Confirmacion: () =>
+				import("components/nuevoPerfil/Confirmacion.vue"),
 		},
 		data() {
 			return {};
@@ -101,6 +104,7 @@
 				"foto",
 				"step",
 			]),
+			...mapGetters("perfiles", ["buscarPerfil"]),
 		},
 		methods: {
 			...mapMutations("perfiles", [
@@ -111,71 +115,20 @@
 				"updateFoto",
 				"updateStep",
 			]),
-			async guardarPerfil() {
-				const perfil = {
-					genero: this.genero,
-					foto: !!this.foto,
-					nombre: this.nombre,
-					apellido: this.apellido,
-					fecha: this.fecha,
-					nivel: 0,
-					exp: 0,
-					tiempo: 0,
-					logros: [],
-					estadisticas: {
-						basico: {
-							tiempo: 0,
-							correctas: 0,
-							incorrectas: 0,
-							pasadas: 0,
-							poderes: 0,
-							logros: [],
-						},
-						intermedio: {
-							tiempo: 0,
-							correctas: 0,
-							incorrectas: 0,
-							pasadas: 0,
-							poderes: 0,
-							logros: [],
-						},
-						avanzado: {
-							tiempo: 0,
-							correctas: 0,
-							incorrectas: 0,
-							pasadas: 0,
-							poderes: 0,
-							logros: [],
-						},
-					},
-				};
-				try {
-					let resultado = await this.$db.local.rel.save(
-						"perfil",
-						perfil
-					);
-					this.$q.notify({
-						type: !!resultado ? "positive" : "negative",
-						message: !!resultado
-							? "Perfil creado"
-							: "No se pudo crear el perfil",
-						timeout: 500,
-					});
-					if (!!resultado && !!perfil.foto) {
-						const attachment = this.foto;
-						const fotoResultado = await this.$db.local.rel.putAttachment(
-							"perfil",
-							{ id: resultado.id, rev: resultado.rev },
-							"file",
-							attachment,
-							attachment.type
-						);
-						if (!!fotoResultado)
-							this.$router.push({ name: "Perfiles" });
-					} else this.$router.push({ name: "Perfiles" });
-				} catch (e) {
-					alert("ha ocurrido un error al crear el perfil: " + e);
+			...mapActions("perfiles", ["cargarPerfiles"]),
+			async crearNuevoPerfil() {
+				let perfil = new Perfil(
+					this.genero,
+					this.nombre,
+					this.apellido,
+					this.fecha
+				);
+				let resultado = await perfil.guardarPerfil();
+				if (!!resultado && !!this.foto) {
+					await perfil.guardarFoto(this.foto);
+					await perfil.establecerFotoPerfil(this.foto.name);
 				}
+				this.$router.push({ name: "Perfiles" });
 			},
 		},
 	};
