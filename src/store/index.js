1827200a6827215b041f3aac7e1752059bbd9c44
Marcel <masv122@gmail.com>
2020-08-31 20:26:59 -0400

    refactor(store): se importa la ienda directamente

--- src/store/index.js
+++ src/store/index.js
@@ -9,29 +9,13 @@ import perfiles from "./perfiles";
 
 Vue.use(Vuex);
 
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export default function(/* { ssrContext } */) {
-	const Store = new Vuex.Store({
-		modules: {
-			global,
-			cronometro,
-			play,
-			puntaje,
-			perfiles,
-		},
-
-		// enable strict mode (adds overhead!)
-		// for dev mode only
-		strict: process.env.DEV,
-	});
-
-	return Store;
-}
+export default new Vuex.Store({
+	modules: {
+		global,
+		cronometro,
+		play,
+		puntaje,
+		perfiles,
+	},
+	strict: process.env.DEV,
+});
