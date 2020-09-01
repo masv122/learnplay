0d497850338bdfa7544cc15cb92a3f8ea1461134
Marcel <masv122@gmail.com>
2020-08-31 20:05:30 -0400

    refactor(clase, login): ahora al iniciar el sistema se carga el id del usuario, se busca en la base de datos y si esta se crea un perfil y se guarda

--- src/boot/auth.js
+++ src/boot/auth.js
@@ -1,10 +1,30 @@
 import { LocalStorage } from "quasar";
+import { db } from "boot/pouchdb/index";
+import Perfil from "src/class/perfil";
 
-export default ({ store }) => {
+export default async ({ store }) => {
 	let perfilLocal = LocalStorage.getItem("perfil");
 	if (!!perfilLocal) {
-		let perfil = store.getters["perfiles/buscarPerfil"](perfilLocal.id);
-		if (!!perfil)
-			store.commit("perfiles/updatePerfil", perfil);
+		const resultado = await db.local.rel.find("perfil", perfilLocal.id);
+		let perfil = new Perfil(
+			resultado.perfiles[0].genero,
+			resultado.perfiles[0].nombre,
+			resultado.perfiles[0].apellido,
+			resultado.perfiles[0].fecha,
+			resultado.perfiles[0].fotoPerfil,
+			resultado.perfiles[0].fotos,
+			resultado.perfiles[0].nivel,
+			resultado.perfiles[0].exp,
+			resultado.perfiles[0].tiempo,
+			resultado.perfiles[0].logros,
+			resultado.perfiles[0].estadisticas,
+			resultado.perfiles[0].attachments,
+			resultado.perfiles[0].id,
+			resultado.perfiles[0].rev
+		);
+		if (perfil.fotoPerfil.nombre) {
+			await perfil.actualizarRutaFoto(perfil.fotoPerfil.nombre);
+		}
+		if (!!perfil) store.commit("perfiles/updatePerfil", perfil);
 	}
 };
