744e781ccd65fa7aad459210f61af7eb4d119590
Marcel <masv122@gmail.com>
2020-08-31 20:19:51 -0400

    refactor(estilo, component): estilo cambiado y compoente asincrono agregado

--- src/components/PlayHeader.vue
+++ src/components/PlayHeader.vue
@@ -1,27 +1,22 @@
 <template>
 	<q-header elevated>
-		<div class="row no-wrap hide">
-			<q-toolbar class="col-4">
-				<perfil-miniatura />
-			</q-toolbar>
-			<q-toolbar class="col-6">
-				<q-toolbar-title>Basico</q-toolbar-title>
-			</q-toolbar>
-			<q-toolbar class="col-2">
-				<q-btn @click="reset()" icon="close" flat round dense />
-			</q-toolbar>
-		</div>
+		<q-toolbar>
+			<perfil-miniatura play />
+			<q-toolbar-title>Basico</q-toolbar-title>
+			<q-btn @click="endResumen()" icon="close" flat round dense />
+		</q-toolbar>
 	</q-header>
 </template>
 
 <script>
 	import playScripts from "src/mixins/playScripts";
-	import perfilMiniatura from "components/perfilMiniatura";
 
 	export default {
 		name: "SupHeader",
 		mixins: [playScripts],
-		components: { perfilMiniatura },
+		components: {
+			perfilMiniatura: () => import("components/perfilMiniatura.vue"),
+		},
 		data() {
 			return {
 				value: 81,
